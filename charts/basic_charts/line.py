2493905f25e0592d377a3050b8ef02321dba7c3e
sirluan <2783697900@qq.com>
2025-07-30 17:40:49 +0800

    refactor(charts): 优化折线图生成逻辑
    - 修正变量名，提高代码可读性
    - 添加 toolbox_opts 配置，支持自定义工具箱显示
    - 改进 y 轴数据处理，支持多条折线绘制
    - 优化代码结构，提高可维护性

--- charts/basic_charts/line.py
+++ charts/basic_charts/line.py
@@ -8,16 +8,17 @@ def line():
     折线图生成工具，生成折线图
     '''
 def plot_line(plot_args,options):
-    bar = Line()
+    line = Line()
     if options:
-        bar.set_global_opts(
+        line.set_global_opts(
             title_opts=opts.TitleOpts(title=options[0], subtitle=options[1]),
             xaxis_opts=opts.AxisOpts(name=options[2]),
             yaxis_opts=opts.AxisOpts(name=options[3]),
-            legend_opts=opts.LegendOpts(pos_left=options[4], pos_top=options[5]))
-        if options[6] != 'False': bar.set_global_opts(opts.ToolboxOpts())
-    bar.add_xaxis(plot_args['x_axis'])
-    bar.add_yaxis(plot_args['title'],plot_args['y_axis'])
-    if 'render' in plot_args: bar.render(plot_args['render'])
-    else: bar.render("line.html")
+            legend_opts=opts.LegendOpts(pos_left=options[4], pos_top=options[5]),
+            toolbox_opts=opts.ToolboxOpts(is_show=options[6] != 'False'))
+    line.add_xaxis(plot_args['x_axis'])
+    for k,v in plot_args['y_axis'].items():
+        line.add_yaxis(k,v)
+    if 'render' in plot_args: line.render(plot_args['render'])
+    else: line.render("line.html")
     return '折线图生成成功'
